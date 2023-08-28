986b62105657325b94ee469842cd3c63244dd8fc
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-08-28 13:18:39 +0300

    Handling Image Inventory on Update

--- Bulky.Data/Repository/ProductRepository.cs
+++ Bulky.Data/Repository/ProductRepository.cs
@@ -18,6 +18,29 @@ namespace Bulky.Data.Repository
             this.dbContext = dbContext;
         }
 
-        public void Update(Product product) => dbContext.Update(product);
+        // public void Update(Product product) => dbContext.Update(product);
+
+        // product update - explicit
+        public void Update(Product obj)
+        {
+            var product = dbContext.Products.FirstOrDefault(p => p.Id == obj.Id);
+            if (product != null)
+            {
+                product.Title = obj.Title;
+                product.ISBN = obj.ISBN;
+                product.Price = obj.Price;
+                product.Price50 = obj.Price50;
+                product.ListPrice = obj.ListPrice;
+                product.Price100 = obj.Price100;
+                product.Description = obj.Description;
+                product.CategoryId = obj.CategoryId;
+                product.Author = obj.Author;
+
+                if (obj.ImageUrl != null)
+                {
+                    product.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
     }
 }
