36901f6f790d0b1d33d0a58b5dde63a2b87da6f6
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-08-31 09:26:22 +0300

    Extended Identity User

--- Bulky.Data/Data/ApplicationDbContext.cs
+++ Bulky.Data/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Bulky.Data.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
