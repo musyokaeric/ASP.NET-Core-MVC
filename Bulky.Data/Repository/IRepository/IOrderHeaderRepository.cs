5a35128c821914b08aad609ae9eb3ae2cacf1241
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-09-11 20:33:36 +0300

    Added OrderHeader helper methods

--- Bulky.Data/Repository/IRepository/IOrderHeaderRepository.cs
+++ Bulky.Data/Repository/IRepository/IOrderHeaderRepository.cs
@@ -10,5 +10,7 @@ namespace Bulky.Data.Repository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader orderHeader);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
     }
 }
--- Bulky.Data/Repository/OrderHeaderRepository.cs
+++ Bulky.Data/Repository/OrderHeaderRepository.cs
@@ -19,5 +19,32 @@ namespace Bulky.Data.Repository
         }
 
         public void Update(OrderHeader orderHeader) => dbContext.Update(orderHeader);
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var order = dbContext.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (order != null)
+            {
+                order.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    order.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var order = dbContext.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                order.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                order.PaymentIntentId = paymentIntentId;
+                order.PaymentDate = DateTime.Now;
+            }
+        }
     }
 }
