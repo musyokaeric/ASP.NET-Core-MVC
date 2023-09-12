71a96c39f7ef36d354954a97ae626840bed4731f
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-09-12 18:15:28 +0300

    Order List UI

---
+++ Bulky.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Bulky.Data.Repository;
+using Bulky.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var orderHeaders = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
