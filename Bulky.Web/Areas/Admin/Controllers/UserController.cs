059c0861c7b68aec9b88616b6b68af14079eceb0
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-09-13 18:02:23 +0300

    Display User List

---
+++ Bulky.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Bulky.Data.Data;
+using Bulky.Data.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = dbContext.applicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = dbContext.UserRoles.ToList();
+            var roles = dbContext.Roles.ToList();
+
+            foreach (var user in users)
+            {
+                if (user.Company == null)
+                {
+                    user.Company = new Company() { Name = "" };
+                }
+
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+            }
+            return Json(new { data = users });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Delete successfull" });
+        }
+
+        #endregion
+    }
+
+}
